943b4044422c16925f53884c2a7a5f182aecf755
Samuel Yemane Ayele <samzhab@gmail.com>
2017-11-28 19:28:52 +0100

    added regex conditional collection of json elements to remove (eg. phys removes all nodes with phys)

--- d3_js_heb_reduced/json_shortner.rb
+++ d3_js_heb_reduced/json_shortner.rb
@@ -2,13 +2,31 @@
 require 'json'
 old_file = File.read('flare.json')
 parsed_json = JSON.parse(old_file)
+array_to_json = []
 max = parsed_json.size - 1
 range = 0..max
-range.each do |member|
-  break if member > 4
-  name = parsed_json[member]['name'].to_s
-  size = parsed_json[member]['size'].to_i
-  puts name.inspect
-  puts '/*/**/*/*/*/'
-  puts size.inspect
+range.each do |count|
+  break if count > 4
+  names_to_keep = []
+  imports_to_keep = []
+  sizes_to_keep = []
+  name = parsed_json[count]['name'].to_s
+  size = parsed_json[count]['size'].to_i
+  next if name[/(.phys.)/]
+  temp_keep = []
+  names_to_keep << parsed_json[count]['name']
+  sizes_to_keep << size.to_i if size
+  parsed_json[count]['imports'].each do |member|
+    temp_keep << member.to_s unless member[/(.phys.)/]
+  end
+  imports_to_keep << temp_keep
+  deep_range = 0..names_to_keep.size - 1
+  deep_range.each do |i|
+    array_to_json << { name: (names_to_keep[i]).to_s,
+                       size: sizes_to_keep[i],
+                       imports: imports_to_keep[i] }
+    puts array_to_json.inspect
+    new_file = File.open('new_flare.json', 'w')
+    new_file.puts array_to_json.to_json
+  end
 end
