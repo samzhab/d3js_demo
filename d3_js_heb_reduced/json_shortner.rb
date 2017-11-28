1cbb3de8e9b127a7266a7f75ce824f7d8351d244
Samuel Yemane Ayele <samzhab@gmail.com>
2017-11-28 19:53:58 +0100

    back to regex into conditions

--- d3_js_heb_reduced/json_shortner.rb
+++ d3_js_heb_reduced/json_shortner.rb
@@ -12,13 +12,12 @@ range.each do |count|
   sizes_to_keep = []
   name = parsed_json[count]['name'].to_s
   size = parsed_json[count]['size'].to_i
-  regex = '/(.phys.)/'
-  next if name[regex.to_s]
+  next if name[/(.phys.)/]
   temp_keep = []
   names_to_keep << parsed_json[count]['name']
   sizes_to_keep << size.to_i if size
   parsed_json[count]['imports'].each do |member|
-    temp_keep << member.to_s unless member[regex.to_s]
+    temp_keep << member.to_s unless member[/(.phys.)/]
   end
   imports_to_keep << temp_keep
   deep_range = 0..names_to_keep.size - 1
