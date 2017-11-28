88a0dc6a07ea64b3fbfabf97a209e253e6be9a11
Samuel Yemane Ayele <samzhab@gmail.com>
2017-11-28 18:55:41 +0100

    added Rubocop to Gemfile | rubocopped

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 
 git_source(:github) do |repo_name|
-  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
+  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?('/')
   "https://github.com/#{repo_name}.git"
 end
 gem 'json', '~> 2.1.0'
+gem 'rubocop', '~> 0.49.0', require: false
--- d3_js_heb_reduced/json_shortner.rb
+++ d3_js_heb_reduced/json_shortner.rb
@@ -2,18 +2,13 @@
 require 'json'
 old_file = File.read('flare.json')
 parsed_json = JSON.parse(old_file)
-new_json_array = []
-array_to_json = []
 max = parsed_json.size - 1
 range = 0..max
-range.each do |range|
-  break if range > 4
-  names_to_keep = []
-  imports_to_keep = []
-  sizes_to_keep = []
-  name = parsed_json[range]['name'].to_s
-  size = parsed_json[range]['size'].to_i
+range.each do |member|
+  break if member > 4
+  name = parsed_json[member]['name'].to_s
+  size = parsed_json[member]['size'].to_i
   puts name.inspect
   puts '/*/**/*/*/*/'
-  puts name.size
+  puts size.inspect
 end
